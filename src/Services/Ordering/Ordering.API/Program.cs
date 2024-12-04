5d4920bdfe2a6e65a56692bad38ddda14f302000
tamaz_mirianashvili <tamazi@DESKTOP-2KFTA3O>
2024-12-04 16:20:57 +0400

    new changes

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Instrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the Container
-
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
+
 
 //Configure the HTTP Request Pipeline
 
