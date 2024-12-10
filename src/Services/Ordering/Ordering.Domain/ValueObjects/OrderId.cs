4b2cb1d1a17907855ef4663af5ecc03b8f8fa244
goat_hiker <tamazimirianashvili@gmail.com>
2024-12-10 12:23:12 +0400

    added domain objects and different events according to Order Domain

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -3,4 +3,17 @@ namespace Ordering.Domain.ValueObjects;
 public record OrderId
 {
     public Guid Value { get;}
+    
+    private OrderId(Guid value) => Value = value;
+
+    public static OrderId Of(Guid orderId)
+    {
+        ArgumentNullException.ThrowIfNull(orderId);
+        if (orderId == Guid.Empty)
+        {
+            throw new DomainException("OrderId cannot be empty");
+        }
+        return new OrderId(orderId);
+    }
+    
 };
--- src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
@@ -3,4 +3,17 @@ namespace Ordering.Domain.ValueObjects;
 public record ProductId
 {
     public Guid Value { get;}
+    
+    private ProductId(Guid value) => Value = value;
+
+    public static ProductId Of(ProductId productId)
+    {
+        ArgumentNullException.ThrowIfNull(productId);
+        if (productId.Value == Guid.Empty)
+        {
+            throw new ArgumentException("Product id is invalid", nameof(productId));
+        }
+
+        return new ProductId(productId.Value);
+    }
 };
