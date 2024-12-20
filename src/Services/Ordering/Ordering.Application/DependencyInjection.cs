ab20e4ad4707c247919006ee1709764e5bb97a07
tamaz_mirianashvili <tamazi@DESKTOP-2KFTA3O>
2024-12-20 16:27:58 +0400

    new changes

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
 
 
-namespace Ordering.Application
+namespace Ordering.Application;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        services.AddMediatR(cfg =>
         {
-            services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
-                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));    
-            });
-            return services;
-        }
+            cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));    
+        });
+        services.AddFeatureManagement();
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+
+
+        return services;
     }
 }
